a0b9ac6815654b9ec494cd0fe6e29f4b686a06da
L-bin113 <76483058+L-bin113@users.noreply.github.com>
2024-06-24 20:56:31 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import LayOut from './components/LayOut.vue'
+</script>
+
+<template>
+
+  <LayOut/>
+
+</template>
+
+<style scoped>
+
+</style>
