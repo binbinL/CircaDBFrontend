440ce66413b4c608fd25d3a487c6d7b5b23e9eb9
L-bin113 <76483058+L-bin113@users.noreply.github.com>
2024-06-27 20:30:26 +0800

    feat:api/trans

--- src/views/Home/components/Left.vue
+++ src/views/Home/components/Left.vue
@@ -2,14 +2,14 @@
   <div class="container">
 
     <div class="vertical-radio-group">
-      <el-select v-model="species" placeholder="please select Species">
+      <el-select v-model="data.species" placeholder="please select Species">
         <el-option label="Human" value="Human"/>
         <el-option label="Mus" value="Mus"/>
       </el-select>
     </div>
 
     <div style="margin-top: 20px" class="vertical-radio-group">
-      <el-radio-group v-model="omics">
+      <el-radio-group v-model="data.omics">
         <el-radio-button label="Transcriptome" value="Transcriptome"/>
         <el-radio-button label="Metabolome" value="Metabolome"/>
         <el-radio-button label="Acetylome" value="Acetylome"/>
@@ -18,23 +18,63 @@
     </div>
 
     <div style="margin-top: 20px" class="vertical-radio-group">
-      <el-select v-model="tissue" placeholder="please select Tissue">
-        <el-option label="Liver" value="Human"/>
-        <el-option label="Brain" value="Mus"/>
+      <el-select v-model="data.tissue" placeholder="please select Tissue">
+        <el-option
+            v-for="(item, key) in tissues"
+            :key="key"
+            :label="key"
+            :value="key"
+        />
+
+        <!--                <el-option label="Liver" value="Liver"/>-->
+        <!--                <el-option label="Brain" value="Brain"/>-->
       </el-select>
     </div>
+
+    <!--    <div style="margin-top: 20px" class="vertical-radio-group">-->
+    <!--      <el-autocomplete-->
+    <!--          v-model="form.name"-->
+    <!--          :fetch-suggestions="querySearch"-->
+    <!--          clearable-->
+    <!--          placeholder="Please Input"-->
+    <!--          @select="handleSelect"-->
+    <!--      />-->
+    <!--    </div>-->
 
 
   </div>
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue'
+import {watch, reactive, ref} from 'vue'
+import axios from 'axios'
 
+const data = reactive({
+  species: 'Mus',
+  omics: 'Transcriptome',
+  tissue: 'Liver'
+})
 
-const omics = ref('Transcriptome')
-const species = ref('Mus')
-const tissue = ref('Human')
+const tissues = ref({})
+
+watch(() => data.species, (newValue, oldValue) => {
+  console.log(newValue, oldValue)
+})
+watch(() => data.omics, (newValue, oldValue) => {
+  console.log(newValue, oldValue)
+  axios.get(`/api/${data.omics}`)
+      .then(response => {
+        console.log(response.data);
+        tissues.value = response.data
+      })
+      .catch(error => {
+        console.error(error);
+      })
+})
+watch(() => data.tissue, (newValue, oldValue) => {
+  console.log(newValue, oldValue)
+})
+
 
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 //elementPlus 按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [
-        // 1. 配置elementPlus采用sass样式配色系统
-        ElementPlusResolver(),
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [
+                // 1. 配置elementPlus采用sass样式配色系统
+                ElementPlusResolver(),
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 })
